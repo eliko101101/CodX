84c0ee9afe3d469ff83d6c6fd76ede461d13f3e5
eliko101101 <eliko101101@gmail.com>
2022-06-03 14:47:56 +0300

    only desktop version

---
+++ js/main.js
@@ -0,0 +1,43 @@
+//=======tabs==============================
+function openWorker(evt, workerName) {
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(workerName).style.display = "flex";
+    evt.currentTarget.className += " active";
+  }
+  function openDirection(evt, directionName) {
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent_1");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks_1");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(directionName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+//==================accordion===============
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
---
+++ js/slider.js
@@ -0,0 +1,8 @@
+$('.slider').slick({
+    slidesToShow: 9,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows:false,
+
+  });
