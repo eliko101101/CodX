9c7387e88151eacd0a15c69b6d2b6ce7a4b363b7
eliko101101 <eliko101101@gmail.com>
2022-06-05 13:12:27 +0300

    mobile version

--- js/slider.js
+++ js/slider.js
@@ -4,5 +4,26 @@ $('.slider').slick({
     autoplay: true,
     autoplaySpeed: 1000,
     arrows:false,
-
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 9,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 6,
+          
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
